c90dbceaf1b754a00e0616edcefd5f2046b04855
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-20 13:23:54 +0530

    [Ranjith] modified Emp_wages.java

--- Emp_wages.java
+++ Emp_wages.java
@@ -1,14 +1,16 @@
 import java.util.*;
-class Employee_present_not{
-	public static final int part_time=1;
+class Emp_wages{
+public static final int part_time=1;
 	public static final int isFull_time=2;
 	public static final int emp_rate_per_hrs=20;
-	public static void main(String args[])
-	{
+	public static final int num_of_work=2;
+	public static void main(String args[]){
 		int emp_hrs=0;
 		int emp_wages=0;
-		int check=(int)Math.floor(Math.random()*10)%3;
-		switch(check){
+		int total_wages=0;
+		for(int day=0;day<num_of_work;day++){
+		  int check=(int)Math.floor(Math.random()*10)%3;
+		  switch(check){
 			case 1: System.out.println("employees is part time hours");
 			emp_hrs=4;
 			break;
@@ -18,7 +20,10 @@ class Employee_present_not{
 			default: System.out.println("the employee is obsent");
 			emp_hrs=0;
 		}
-		emp_wages=emp_hrs*emp_rate_per_hrs;
-		System.out.println(emp_wages);
+			emp_wages=emp_hrs*emp_rate_per_hrs;
+			total_wages+=emp_wages;
+			System.out.println(emp_wages);
+		}
+		System.out.println("total hours of Employee"+" "+total_wages);
 	}
 }
