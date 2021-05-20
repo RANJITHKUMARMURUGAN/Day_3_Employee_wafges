0cabc8db5e318844fde2786e18de601910462ae2
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-20 12:47:53 +0530

    [ranjith] modified Emp_wages.java

--- Emp_wages.java
+++ Emp_wages.java
@@ -1,1 +1,13 @@
 import java.util.*;
+class Employee_present_not{
+	public static void main(String args[]){
+		int isFull_time=1;
+		double check=Math.floor(Math.random()*10)%2;
+		if(isFull_time==check){
+			System.out.println("Employee is present");
+		}
+		else{
+			System.out.println("Employee is not present");
+		}
+	}
+}
