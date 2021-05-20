41a721fbc48af3e80307b36356ac954cbe414a7c
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-20 13:08:57 +0530

    [Ranjith] modified Emp_wages.java

--- Emp_wages.java
+++ Emp_wages.java
@@ -1,16 +1,20 @@
 import java.util.*;
 class Employee_present_not{
 	public static void main(String args[]){
-		int isFull_time=1;
+		int part_time=1;
+		int isFull_time=2;
 		int emp_rate_per_hrs=20;
 		int emp_hrs=0;
 		int emp_wages=0;
 		double check=Math.floor(Math.random()*10)%2;
-		if(isFull_time==check){
+		if(part_time==check){
+			emp_hrs=4;
+		}
+		else if(isFull_time==check){
 			emp_hrs=8;
 		}
 		else{
-			emp_hrs=4;
+			emp_hrs=0;
 		}
 		emp_wages=emp_hrs*emp_rate_per_hrs;
 		System.out.println(emp_wages);
